7bdbc40f9823dc8b390a4ed80dc7a848fffc0e82
Seth Burns <seth@sethburns.com>
2023-07-31 18:45:29 -0700

    Add date filter and some styling.

--- src/components/SearchSelect/SeachSelect.js
+++ src/components/SearchSelect/SeachSelect.js
@@ -4,35 +4,46 @@ import { locations } from '../stationData';
 import React from 'react';
 
 export const SearchSelect = () => {
-
+  const today = new Date();
+  const formattedDate = today.toISOString().slice(0, 10);
   const [searchLocation, setSearchLocation] = useState('');
-  // const [startDate, setStartDate] = useState('');
-  // const [endDate, setEndDate] = useState('');
+  const [dateSelect, setDateSelect] = useState(formattedDate);
 
-  
-
-  const filteredResults = Object.keys(locations).map(area => {
-    return locations[area].filter((place) => place.name.toLowerCase().includes(searchLocation.toLowerCase())).map((location) => {
-      return (
-        <button className='location-button' key={[location.latitude, location.longitude]}>
-          <p>{location.name}</p>
-        </button>
+  const filteredResults = Object.keys(locations).map((area) => {
+    return locations[area]
+      .filter((place) =>
+        place.name.toLowerCase().includes(searchLocation.toLowerCase())
       )
-    })
-  })
-   
+      .map((location) => {
+        return (
+          <button
+            className="location-button"
+            key={[location.latitude, location.longitude]}
+          >
+            <p>{location.name}</p>
+          </button>
+        );
+      });
+  });
+
 
   return (
     <div>
-      <form>
+      <form className="search-form">
         <input
           type="text"
           placeholder="Search Location Name"
           value={searchLocation}
           onChange={(e) => setSearchLocation(e.target.value)}
         ></input>
+        <input
+          className="date-input"
+          value={dateSelect}
+          type="date"
+          onChange={(e) => setDateSelect(e.target.value)}
+        ></input>
       </form>
-      <ul className='location-buttons'>{filteredResults}</ul>
+      <ul className="location-buttons">{filteredResults}</ul>
     </div>
   );
 };
