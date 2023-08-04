de03d1b58685fcc758c13da6b479ccdd44ba613f
Seth Burns <seth@sethburns.com>
2023-08-04 12:12:42 -0700

    Add Cypress testing for main page

--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -1,5 +1,41 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('homepage search page spec', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3000/sockjs-node/info?t=*', {
+      statusCode: 200,
+      fixture: 'websocket',
+    });
+  });
+  it('navigates to pages and loads a small form, buttons, and a list that the form should filter', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('.navbar')
+      .find('.navbutton')
+      .eq(1)
+      .click();
+    cy.get('.saved-tides-page').contains('h1', 'SAVED TIDES');
+    cy.get('.saved-tides-page').contains(
+      '.no-tides',
+      'You have no saved tides!'
+    );
+    cy.get('.navbar')
+      .find('.navbutton')
+      .eq(0)
+      .click();
+    cy.get('h1').should('contain', 'tideWAtch');
+    cy.get('.navbutton')
+      .eq(0)
+      .contains('p', 'Search');
+    cy.get('.navbutton')
+      .eq(1)
+      .contains('p', 'Saved');
+    cy.get('ul')
+      .find('li')
+      .should('have.length', 186);
+    cy.get('#searchInput').type('long');
+    cy.get('#searchInput').should('have.value', 'long');
+    cy.get('ul')
+      .find('li')
+      .should('have.length', 3);
+    cy.get('#dateInput').type('2023-08-10');
+    cy.get('#dateInput').should('have.value', '2023-08-10');
+  });
+});
--- src/components/ServerDown/ServerDown.js
+++ src/components/ServerDown/ServerDown.js
@@ -1,9 +1,12 @@
 import './ServerDown.css'
 import React from 'react'
+import image from '../../images/ServerDown.png'
 export const ServerDown = () => {
+
+  
   return (
-    <div>
-      <img src='../../images/ServerDown.png' alt='Server is down, sailboat sails and the sun spelling out four oh four.' />
+    <div className='server-down'>
+      <img src={image} alt='Server is down, sailboat sails and the sun spelling out four oh four.' />
     </div>
   )
 }
